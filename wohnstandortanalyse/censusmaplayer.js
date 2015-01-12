49637b00535b269d9471032b988c6ad15ca74add
harbar <harald.baron@hotmail.com>
2015-01-12 21:08:32 +0100

    Update censusmaplayer.js

--- wohnstandortanalyse/censusmaplayer.js
+++ wohnstandortanalyse/censusmaplayer.js
@@ -110,7 +110,7 @@ form.onsubmit = function(evt) {
       geolocation.setTracking(true); // here the browser may ask for confirmation
       geolocation.on('change', function() {
         geolocation.setTracking(false);
-        olMap.getView().fitGeometry(geolocation.getAccuracyGeometry(), map.getSize(), {maxZoom: 18});
+        olMap.getView().fitGeometry(geolocation.getAccuracyGeometry(), olMap.getSize(), {maxZoom: 18});
         marker.setGeometry(new ol.geom.Point(olMap.getView().getCenter()));
       });
    }
