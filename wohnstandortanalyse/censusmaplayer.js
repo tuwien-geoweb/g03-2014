820b71a6eaa596b0aa445a38ebcd7b2ea646a776
Boerd <viktor.vogler@hotmail.de>
2015-01-12 08:19:11 +0100

    Update censusmaplayer.js

--- wohnstandortanalyse/censusmaplayer.js
+++ wohnstandortanalyse/censusmaplayer.js
@@ -223,7 +223,11 @@ document.getElementById('layer_gruenflaechen').onclick = function(e){
     olMap.addLayer(l_gruenflaechen);
     olMap.removeLayer(wmsLayer);
     }else{
+      if(wmsLayervisible=='1'){
+        olMap.removeLayer(l_gruenflaechen);
+      }else{
       olMap.removeLayer(l_gruenflaechen);
+      olMap.addLayer(wmsLayer);
     }
   };  
   
